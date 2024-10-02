5bb7c13bf0666d66dd465e03f0534d72f3dd865e
Christopher Chen <christopher.chen444@gmail.com>
2024-10-02 16:47:04 -0700

    first commit!

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -2,10 +2,10 @@
 # This is a practice script for the Husky Coding Project's Git
 # and Command Line workshop. 
 
-NAME="HELLO WORLD" # Change This Variable to Your Name
+NAME="Christopher Chen" # Change This Variable to Your Name
 
 validate_name() {
-    if [ "$NAME" = "HELLO WORLD" ]; then
+    if [ "NAME" = "HELLO WORLD" ]; then
         echo "You Haven't Changed the NAME Variable on Line 5. Please Change it!"
     else
         echo "Good Job! You set your name to $NAME"
